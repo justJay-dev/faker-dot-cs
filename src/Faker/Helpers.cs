82353e085438f70c0af6f5ba2c2ca20061d205b9
Jay Simpson <jay_simpson@unigroup.com>
2021-11-10 09:50:56 -0500

    implements randomize

--- src/Faker/Helpers.cs
+++ src/Faker/Helpers.cs
@@ -1,37 +1,57 @@
 using System;
-namespace Faker.Helpers
+using System.Collections.Generic;
+namespace Faker
+
 {
-	public class Repeat
-	{        /// <summary>
-			 /// Repeat a given function(action) (count) amount of times
-			 /// </summary>
-		public static void Times(int count, Action action)
-		{
-			for (int i = 0; i < count; i++)
-			{
-				action();
-			}
-		}
-	}
-	public class Pick
+	public class Helpers
 	{
 		/// <summary>
-		/// select a value at random from a given list 
+		/// select a value at random from a given list or array
 		/// </summary>
-		public static dynamic RandomList(dynamic list)
+		public static dynamic Randomize(object listOrArray)
 		{
-
-			var random = new Random();
-			return list[random.Next(list.Count)];
+			if (listOrArray.GetType().IsArray)
+			{
+				return Pick.RandomArray(listOrArray);
+			}
+			else
+			{
+				return Pick.RandomList(listOrArray);
+			}
 		}
-		/// <summary>
-		/// select an element at random from a given array 
-		/// </summary>
-		public static dynamic RandomArray(dynamic array)
+		public class Repeat
+		{        /// <summary>
+				 /// Repeat a given function(action) (count) amount of times
+				 /// </summary>
+			public static void Times(int count, Action action)
+			{
+				for (int i = 0; i < count; i++)
+				{
+					action();
+				}
+			}
+		}
+		public class Pick
 		{
+			/// <summary>
+			/// select a value at random from a given list 
+			/// </summary>
+			public static dynamic RandomList(dynamic list)
+			{
 
-			var random = new Random();
-			return array[random.Next(0, array.Length)];
+				var random = new Random();
+				return list[random.Next(list.Count)];
+			}
+			/// <summary>
+			/// select an element at random from a given array 
+			/// </summary>
+			public static dynamic RandomArray(dynamic array)
+			{
+
+				var random = new Random();
+				return array[random.Next(0, array.Length)];
+			}
 		}
+
 	}
 }
