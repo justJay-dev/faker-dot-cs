5338197d98eb378418f3ba56472bfc031dee840e
Jay Simpson <jay_simpson@unigroup.com>
2021-11-10 10:31:43 -0500

    imeplements company method

---
+++ src/Faker/Locales/en/Company/BSNouns.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+namespace Faker.Locales
+{
+	public partial class Company
+	{
+		public List<string> BSNouns = new List<string>
+		{
+			"synergies",
+			"web-readiness",
+			"paradigms",
+			"markets",
+			"partnerships",
+			"infrastructures",
+			"platforms",
+			"initiatives",
+			"channels",
+			"eyeballs",
+			"communities",
+			"ROI",
+			"solutions",
+			"e-tailers",
+			"e-services",
+			"action-items",
+			"portals",
+			"niches",
+			"technologies",
+			"content",
+			"vortals",
+			"supply-chains",
+			"convergence",
+			"relationships",
+			"architectures",
+			"interfaces",
+			"e-markets",
+			"e-commerce",
+			"systems",
+			"bandwidth",
+			"infomediaries",
+			"models",
+			"mindshare",
+			"deliverables",
+			"users",
+			"schemas",
+			"networks",
+			"applications",
+			"metrics",
+			"e-business",
+			"functionalities",
+			"experiences",
+			"web services",
+			"methodologies",
+			"blockchains"
+		};
+	}
+}
