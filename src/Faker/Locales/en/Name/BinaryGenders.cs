6c83ba6687242ff44ea99da7143df556c393a04c
Jay Simpson <jay_simpson@unigroup.com>
2021-11-12 13:22:03 -0500

    implements name method

---
+++ src/Faker/Locales/en/Name/BinaryGenders.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+namespace Faker.Locales
+{
+	public partial class Name
+	{
+		public List<string> BinaryGenders = new List<string>
+		{
+			"Female",
+			"Male"
+		};
+	}
+}
---
+++ src/Faker/Locales/en/Name/Suffixes.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+namespace Faker.Locales
+{
+	public partial class Name
+	{
+		public List<string> Suffixes = new List<string>
+		{
+			"Jr.",
+			"Sr.",
+			"I",
+			"II",
+			"III",
+			"IV",
+			"V",
+			"MD",
+			"DDS",
+			"PhD",
+			"DVM"
+		};
+	}
+}
