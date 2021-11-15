5bf23d1f563f34de97ea6c860f1adfb24bc55627
Jay <jay@lowbrowlabs.com>
2021-11-15 13:55:48 -0500

    typo

--- src/Faker/Date.cs
+++ src/Faker/Date.cs
@@ -21,14 +21,14 @@ namespace Faker
             return DateTime.Parse(randMonth + "/" + randDay + "/" + randYear);
         }
 
-        public static DateTime Recent(int days = 15, string refDate = "MMM/DD/YYYY")
+        public static DateTime Recent(int days = 15, string refDate = "MM/DD/YYYY")
         {
             int random = Datatype.Int(1, days);
             DateTime startHere = refDate == "MM/DD/YYYY" ? DateTime.Today : DateTime.Parse(refDate);
             return startHere.AddDays(random * -1);
 
         }
-        public static DateTime Soon(int days = 15, string refDate = "MMM/DD/YYYY")
+        public static DateTime Soon(int days = 15, string refDate = "MM/DD/YYYY")
         {
             int random = Datatype.Int(1, days);
             DateTime startHere = refDate == "MM/DD/YYYY" ? DateTime.Today : DateTime.Parse(refDate);
