3a7e0940836b3d3bd8c9f3014c004d6eddd6c9e7
Jay <jay@lowbrowlabs.com>
2022-12-16 12:20:09 -0500

    summaries

--- src/Faker/Database.cs
+++ src/Faker/Database.cs
@@ -2,24 +2,36 @@ namespace Faker
 {
     public class Database
     {
+        /// <summary>
+        /// Returns a random column name as a string
+        /// </summary>
         public static string Column()
         {
             var database = new Locales.Database();
             return Helpers.Randomize(database.Columns);
         }
 
+        /// <summary>
+        /// Returns a random column type as a string
+        /// </summary>
         public static string Type()
         {
 
             var database = new Locales.Database();
             return Helpers.Randomize(database.Types);
         }
+        /// <summary>
+        /// Returns a random collation value as a string
+        /// </summary>
         public static string Collation()
         {
 
             var database = new Locales.Database();
             return Helpers.Randomize(database.Collations);
         }
+        /// <summary>
+        /// Returns a random engine value as a string
+        /// </summary>
         public static string Engine()
         {
 
